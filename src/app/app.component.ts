b7927133a0768ed96421c93fe4251d130d6d4ec6
koderoom <koderoom.dev@gmail.com>
2020-09-01 19:27:34 +0530

    ajax.call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { consoleTestResultHandler } from 'tslint/lib/test';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'black-app';
   public list: any = [];
 
@@ -17,9 +18,27 @@ export class AppComponent {
   }*/
   constructor(private http: HttpClient) {}
 
+  public ngOnInit() {
+    console.log('ON.INIT.');
+    this.makeAjaxCall();
+  }
+
   async makeAjaxCall() {
     const url = 'https://jsonplaceholder.typicode.com/posts';
     const results = await this.http.get(url).toPromise();
     this.list = results;
+  }
+
+  async makePostApiCall() {
+    const userJson = {
+      username: 'Akshay Kumar',
+      email: 'akshay@gmail.com',
+      password: '1234',
+      mobile: '1233456',
+    };
+
+    const url = 'http://localhost:3000/adduser';
+    const output = await this.http.post(url, userJson).toPromise();
+    console.log(output);
   }
 }
